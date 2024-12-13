ee8cc4d184c540e213f7d9ce4a2cb6c9f40cb9af
Luna <luna@waya.one>
2024-12-13 17:37:12 +0100

    add bluesky social

--- components/socials/constants.tsx
+++ components/socials/constants.tsx
@@ -2,11 +2,12 @@ import type { ReactNode } from "react";
 import { BsDiscord } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
 import { HiMail } from "react-icons/hi";
-import { SiKofi } from "react-icons/si";
+import { SiBluesky, SiKofi } from "react-icons/si";
 
 export const platforms = {
     discord: <BsDiscord />,
-    github: <FaGithub />,
+    github: <FaGithub className="relative right-0.5 -mr-[1px]" />,
     "ko-fi": <SiKofi />,
-    email: <HiMail />
+    email: <HiMail className="size-5 relative right-0.5 -mr-0.5" />,
+    bluesky: <SiBluesky className="p-[1px]" />
 } satisfies Record<string, ReactNode>;
